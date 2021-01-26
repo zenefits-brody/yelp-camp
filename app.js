6396229f70f5972ac88f6e12c373542ec33e11d6
Brody Chen <rufengc@zenefits.com>
2021-01-25 20:45:40 -0800

    Connect to mongoose

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Campground = require('./models/campground');
+
+mongoose
+  .connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Mongo connection success!');
+  })
+  .catch((error) => {
+    console.log('Mongo connection failed!');
+    console.log(error);
+  });
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
   res.render('home');
+});
+
+app.get('/make', async (req, res) => {
+  const camp = new Campground({ title: 'My Backyard', description: 'cheap' });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
