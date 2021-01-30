f0f97ce8efaa9e42908868c3a25119fe5d5b57b3
Brody Chen <rufengc@zenefits.com>
2021-01-30 13:55:58 -0800

    Add route for new campground

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// For parsing post request body
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', async (req, res) => {
   res.render('home');
 });
@@ -32,9 +35,24 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+// This route needs to be defined before `/campgrounds/:id` route, otherwise we can never enter this route.
+app.get('/campgrounds/new', async (req, res) => {
+  res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
+});
+
+/**
+ * POST routes
+ */
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
