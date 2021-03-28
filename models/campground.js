b78aa76c40d60ef4a1461cd6df0b35acee0282d8
Brody Chen <rufengc@zenefits.com>
2021-03-27 17:05:23 -0700

    Add custom validation error message

--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: [true, 'Title cannot be empty.'] },
   image: String,
   price: Number,
   description: String,
