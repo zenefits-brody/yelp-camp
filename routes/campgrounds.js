e77f11f0328924407b20824d24cf8127beffa504
Brody Chen <rufengc@zenefits.com>
2021-11-17 15:32:12 -0800

    Show author on show page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -38,7 +38,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.get(
   '/:id',
   wrapAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if (!campground) {
       req.flash('error', 'Campground not found.');
       return res.redirect('/campgrounds');
@@ -72,6 +72,7 @@ router.post(
   validateCampground,
   wrapAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'New campground created successfully.');
     res.redirect(`/campgrounds/${campground._id}`);
