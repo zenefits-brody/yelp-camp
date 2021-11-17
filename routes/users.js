b7403d1cdd883ce429ccd3b26cb328edefbfb843
Brody Chen <rufengc@zenefits.com>
2021-11-16 18:47:01 -0800

    Add returnTo to session

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,11 @@ router.post(
   }),
   (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+
+    res.redirect(redirectUrl);
   },
 );
 
